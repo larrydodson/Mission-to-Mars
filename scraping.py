ff69e61dde3a58b33af3e582fbf060563b688c7a
Larry Dodson <67447445+larrydodson@users.noreply.github.com>
2020-09-28 22:30:05 -0500

    Add files via upload

---
+++ scraping.py
@@ -0,0 +1,78 @@
+
+# Module 10 Mars
+# Import Splinter and BeautifulSoup
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+import pandas as pd
+
+# Set the executable path and initialize the chrome browser in splinter
+executable_path = {'executable_path': 'chromedriver'}
+browser = Browser('chrome', **executable_path)
+
+# Path to chromedriver
+#!where chromedriver
+
+# Featured Articles
+# Visit the mars nasa news site
+url = 'https://mars.nasa.gov/news/'
+browser.visit(url)
+
+# Optional delay for loading the page
+browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+
+# set up the HTML parser. Convert the browser html to a soup object and then quit the browser
+html = browser.html
+news_soup = soup(html, 'html.parser')
+
+slide_elem = news_soup.select_one('ul.item_list li.slide')
+
+# assign the title and summary text to variables to reference later. Begin scraping: 
+slide_elem.find("div", class_='content_title')
+
+# Use the parent element to find the first tag and save it as `news_title`
+news_title = slide_elem.find("div", class_='content_title').get_text()
+news_title
+
+# Use the parent element to find the paragraph text
+news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
+news_p
+
+
+# Featured Image JPL
+# Visit URL
+url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+browser.visit(url)
+
+# Find and click the full image button
+full_image_elem = browser.find_by_id('full_image')
+full_image_elem.click()
+
+# Find the more info button and click that
+browser.is_element_present_by_text('more info', wait_time=1)
+more_info_elem = browser.links.find_by_partial_text('more info')
+more_info_elem.click()
+
+# Parse the resulting html with soup
+html = browser.html
+img_soup = soup(html, 'html.parser')
+
+# Find the relative image url
+img_url_rel = img_soup.select_one('figure.lede a img').get("src")
+img_url_rel
+
+# Use the base URL to create an absolute URL
+img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
+img_url
+
+
+# Mars Facts
+df = pd.read_html('http://space-facts.com/mars/')[0]
+df.head()
+
+df.columns=['Description', 'Value']
+df.set_index('Description', inplace=True)
+df
+
+df.to_html()
+
+browser.quit()
