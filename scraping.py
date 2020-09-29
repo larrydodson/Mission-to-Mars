f2eddbdccd67ba824ee4b7dbe96125280f32483c
Larry Dodson <67447445+larrydodson@users.noreply.github.com>
2020-09-29 15:49:09 -0500

    Add files via upload

--- scraping.py
+++ scraping.py
@@ -1,13 +1,32 @@
 
 # Module 10, Mars
-# Import Splinter and BeautifulSoup
+# Import Splinter,  BeautifulSoup and Pandas
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 import pandas as pd
+import datetime as dt
+
+def scrape_all():
+    # Initiate headless driver for deployment (initialize the browser)
+    browser = Browser("chrome", executable_path="chromedriver", headless=False)
+    # set news title and paragraph variables 
+    news_title, news_paragraph = mars_news(browser)
+
+    # Run all scraping functions and store results in a dictionary (create data dictionary)
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+    # Stop webdriver and return scraped data
+    browser.quit()
+    return data
 
 # Set the executable path and initialize the chrome browser in splinter
-executable_path = {'executable_path': 'chromedriver'}
-browser = Browser('chrome', **executable_path)
+#executable_path = {'executable_path': 'chromedriver'}
+#browser = Browser('chrome', **executable_path)
 
 # Path to chromedriver
 #!where chromedriver
@@ -23,7 +42,7 @@ def mars_news(browser):
     # Optional delay for loading the page
     browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
 
-    # set up the HTML parser. Convert the browser html to a soup object and then quit the browser
+    # Set up the HTML parser. Convert the browser html to a soup object and then quit the browser.
     html = browser.html
     news_soup = soup(html, 'html.parser')
 
@@ -89,7 +108,9 @@ def mars_facts():
     df.set_index('Description', inplace=True)
     #df
     # Convert dataframe into HTML format, add bootstrap
-    return df.to_html()
+    return df.to_html(classes="table table-striped")
 
-
-browser.quit()
+# Tell Flask the script is complete and ready. 
+if __name__ == "__main__":
+    # If running as script, print scraped data
+    print(scrape_all())
