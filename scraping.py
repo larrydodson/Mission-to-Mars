f2c7f8013a0ab7f0299190d1b95d5c46d9ba8f10
Larry Dodson <67447445+larrydodson@users.noreply.github.com>
2020-09-30 09:19:21 -0500

    Add files via upload

--- scraping.py
+++ scraping.py
@@ -53,9 +53,9 @@ def mars_news(browser):
         #slide_elem.find("div", class_='content_title')
 
         # Use the parent element to find the first 'a' tag and save it as `news_title`
-        news_title = slide_elem.find("div", class_='content_title').get_text()
+        news_title = slide_elem.find("div", class_="content_title").get_text()
         # Use the parent element to find the paragraph text
-        news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
+        news_p = slide_elem.find("div", class_="article_teaser_body").get_text()
         
     except AttributeError:
         return None, None
