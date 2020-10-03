a579bdfd90ed2bd8bdda245e149556ce0edc45ac
Larry Dodson <67447445+larrydodson@users.noreply.github.com>
2020-10-03 14:18:36 -0500

    Add files via upload

--- scraping.py
+++ scraping.py
@@ -1,10 +1,13 @@
 
-# Module 10, Mars
-# Import Splinter,  BeautifulSoup and Pandas
-from splinter import Browser
+# Module 10, Mission to Mars Challenge
+# Import Splinter,  BeautifulSoup, Pandas, and datetime
+from splinter import Browser, browser
 from bs4 import BeautifulSoup as soup
 import pandas as pd
 import datetime as dt
+import time
+import requests
+import pymongo
 
 def scrape_all():
     # Initiate headless driver for deployment (initialize the browser)
@@ -18,18 +21,14 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
+        "weather": mars_weather(browser),
+        "hemisphere_title": title,
+        "hemispheres": hemisphere_image_urls,
         "last_modified": dt.datetime.now()
     }
     # Stop webdriver and return scraped data
     browser.quit()
     return data
-
-# Set the executable path and initialize the chrome browser in splinter
-#executable_path = {'executable_path': 'chromedriver'}
-#browser = Browser('chrome', **executable_path)
-
-# Path to chromedriver
-#!where chromedriver
 
 # Featured Articles
 def mars_news(browser):
@@ -104,11 +103,85 @@ def mars_facts():
         return None
 
     # Assign columns and set index of dataframe
-    df.columns=['Description', 'Mars']
+    df.columns=['Description', 'Data']
     df.set_index('Description', inplace=True)
     #df
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+# Mars Weather
+def mars_weather(browser):
+    insight_url = 'https://mars.nasa.gov/insight/weather/'
+    browser.visit(insight_url)
+# Parse the data
+    html = browser.html
+    weather_soup = soup(html, 'html.parser')
+# Scrape the Daily Weather Report table
+    weather_table = weather_soup.find('table', class_='mb_table')
+    print(weather_table.prettify())
+
+    #<a class="inline_image_enlarge fancybox" href="https://mars.nasa.gov/rss/api/images/insight_marsweather_white.png">
+#<img alt="Three Day Weather Report" src="https://mars.nasa.gov/rss/api/images/insight_marsweather_white.png">
+# 
+    # Find the relative image url
+    #img_url_rel = weather_soup.select_one('a', class_='inline_image_enlarge fancybox').get('src')
+
+    #weather_img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
+    return weather_table.prettify()
+
+# Deliverable-1, Scrape High-Resolution Mars Hemisphere Images and Titles
+
+# Initiate headless driver for deployment (initialize the browser)
+browser = Browser("chrome", executable_path="chromedriver", headless=False)
+
+# 1. Use browser to visit the URL 
+long_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+browser.visit(long_url)
+# Optional delay for loading the page
+browser.is_element_present_by_css("ul.item_list li.slide", wait_time=2)
+# main_url 
+short_url = 'https://astrogeology.usgs.gov'
+
+# 2. Create a list to hold the images and titles.
+hemisphere_image_urls = []
+
+# 3. Write code to retrieve the image urls and titles for each hemisphere.
+browser.visit(long_url)
+html = browser.html
+hemi_soup = soup(html, 'html.parser')
+main_url = hemi_soup.find_all('div', class_='item')
+titles=[]
+
+# Loop through the items previously stored
+#def mars_hemispheres():
+for i in main_url:
+    hemispheres = {}
+    title = i.find('h3').get_text()
+    url = i.find('a')['href']
+    hem_img_url= short_url+url
+    #print(hem_img_url)
+    browser.visit(hem_img_url)
+    html = browser.html
+    hemi_soup = soup(html, 'html.parser')
+    hemisphere_img_original = hemi_soup.find('div', class_='downloads')
+    hemisphere_img_url = hemisphere_img_original.find('a')['href']
+    # use 'browser.back'  ***  
+    #print(hemisphere_img_url)
+    hemispheres = dict({'title': title, 'img_url': hemisphere_img_url})
+    hemisphere_image_urls.append(hemispheres)
+    browser.back()
+
+
+#    hemispheres = {hemisphere_image_urls}
+#mars_data['hemisphere_image_urls'] = hemisphere_image_urls
+
+    #return mars_data
+
+# 4. Print the list that holds the dictionary of each image url and title.
+    hemisphere_image_urls
+
+# 5. Quit the browser
+browser.quit()
 
 # Tell Flask the script is complete and ready. 
 if __name__ == "__main__":
